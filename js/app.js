4771212a9161446d3ba84007a6848a66fffb1115
Terra Huber <swiftruns26@gmail.com>
2019-07-03 04:49:58 -0500

    JS link isn't working

--- js/app.js
+++ js/app.js
@@ -1,45 +1,72 @@
 "use strict";
 console.log('What do you know about Terra?');
-var welcome;
 var score = 0;
-//Prompt for the first question, checks response and gives alert
+var userName;
+// possibleAnswers=[yes, y, no, n]
 
-var firstAnswer = prompt('What does Terra do for a living?');
+var userName = prompt('What is your name?');
+console.log('What is the users name?');
+console.log(userName);
+/*Prompt for the first question, checks response and gives alert based on response.  
+If player got answer correct, they get a point*/
+
+var firstAnswer = prompt(userName + ', do you think that Terra has been a teacher in only one school?');
+firstAnswer.toLowerCase();
 console.log(firstAnswer.toLowerCase());
-if (firstAnswer === 'teacher') {
-    alert('Correct. Terra is a ' + firstAnswer + ' at Mid-Prairie Middle School.')
+if (firstAnswer === 'n'|| 'no') {
+   alert('Woohoo! You are correct. Terra has taught at three school in the past 18 years!')
+   score++;
 } else {
-    alert('No, Terra is not a(n)' + firstAnswer + '. She is a teacher.')
+    alert('Sorry, ' + userName + 'that is not correct. Terra has taught at three schools in the past 18 years!')
 };
+console.log(score);
+alert('So far, you have '+ score + ' points.')
 
-var secondAnswer = prompt('What sport did Terra participate in college?');
+var secondAnswer = prompt(userName + ', do you think Terra was a collegiate athlete?');
+secondAnswer.toLowerCase();
 console.log(secondAnswer.toLowerCase());
-if (secondAnswer == 'swimming' || 'rugby') {
+if (secondAnswer === 'y' || 'yes') {
     alert('Correct. Terra was on the ' + secondAnswer + ' team at UNI. Go Panthers!')
+    score++;
 } else {
-    alert('Sorry, good guess.  Terra doesn\'t play ' + secondAnswer + '. She swam and played rugby for UNI.')
+    alert('Wa wa. Sorry, but Terra actually swam for UNI as well as rugby for UNI.')
 };
+console.log(score);
+alert(userName + ' you have '+ score + ' points.')
 
-var thirdAnswer = prompt('');
+var thirdAnswer = prompt('Is Terra a mom?');
+thirdAnswer.toLowerCase();
 console.log(thirdAnswer.toLowerCase());
-if (thirdAnswer === '') {
-    alert()
+if (thirdAnswer ==='y' || 'yes') {
+    alert('You rock,' + userName + '. Terra is a mom of two boys.')
+    score++;
 } else {
-    alert()
+    alert('Sorry,' + userName + ' no point for you.')
 };
+console.log(score);
+alert( userName + ', you have '+ score + ' points.')
 
-var fourthAnswer = prompt('');
+var fourthAnswer = prompt('Does Terra bring a Mac to class?');
+fourthAnswer.toLowerCase();
 console.log(fourthAnswer.toLowerCase());
-if (fourthAnswer === '') {
-    alert()
+if (fourthAnswer === 'n' || 'no') {
+    alert( userName + ', you are on FIRE! Terra uses a pc.')
+    score++;
 } else {
-    alert()
+    alert('Silly,' + username + 'Terra doesn\'t bring a Mac.')
 };
+console.log(score);
+alert('You have '+ score + ' points. Last question!')
 
-var fifthAnswer = prompt('');
+var fifthAnswer = prompt('Does Terra drink coffee?');
+fifthAnswer.toLowerCase();
 console.log(fifthAnswer.toLowerCase());
-if (fifthAnswer === '') {
-    alert()
+
+if (fifthAnswer === 'y' || 'yes') {
+    alert(username + ', you guessed right! Terra LOVES coffee!')
+    score++;
 } else {
-    alert()
+    alert('What, who doesn\'t love coffee?')
 };
+console.log(score);
+alert(userName + 'you earned'+ score + ' points.')
