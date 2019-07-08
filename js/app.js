68b1fadb3ea4c2833931a80b87f2c0f1770aa400
Terra Huber <swiftruns26@gmail.com>
2019-07-08 04:34:43 -0500

    Updated code to fix errors question 1-5, added question 6 & 7

--- js/app.js
+++ js/app.js
@@ -10,63 +10,132 @@ console.log(userName);
 /*Prompt for the first question, checks response and gives alert based on response.  
 If player got answer correct, they get a point*/
 
-var firstAnswer = prompt(userName + ', do you think that Terra has been a teacher in only one school?');
-firstAnswer.toLowerCase();
-console.log(firstAnswer.toLowerCase());
-if (firstAnswer === 'n'||'no') {
-   alert('Woohoo! You are correct. Terra has taught at three school in the past 18 years!')
-   score++;
-} else {
-    alert('Sorry, ' + userName + 'that is not correct. Terra has taught at three schools in the past 18 years!')
-};
-console.log(score);
-alert('So far, you have '+ score + ' point.')
+var answer = prompt(userName + ', do you think that Terra has been a teacher in only one school?');
 
-var secondAnswer = prompt(userName + ', do you think Terra was a collegiate athlete?');
-secondAnswer.toLowerCase();
-console.log(secondAnswer.toLowerCase());
-if (secondAnswer === 'y'||'yes') {
-    alert(username + ', you are correct! Terra swam for UNI and played rugby. Go Panthers!')
+console.log(answer.toLowerCase());
+
+
+//made change... added array of possible answers.
+var possibleAnswers = ['y', 'yes', 'n', 'no']
+
+//check to see if userName answer matches answers in array
+if (answer.toLowerCase() === possibleAnswers[2, 3]) {
+    alert('Woohoo! You are correct. Terra has taught at three school in the past 18 years!');
     score++;
 } else {
-    alert('Wa wa. Sorry, but Terra actually swam for UNI as well as rugby for UNI.')
+    alert('Sorry, ' + userName + ' that is not correct. Terra has taught at three schools in the past 18 years!');
 };
+//check to see that they answer doesn't include extra spaces
+//console.log()-- check to see that answer is correct
+console.log('Taught in one building? Expect no');
+console.log(answer.toLowerCase());
+console.log('Points earned?');
 console.log(score);
-alert(userName + ' you have '+ score + ' points.')
+alert('So far, you have ' + score + ' points.');
 
-var thirdAnswer = prompt('Is Terra a mom?');
-thirdAnswer.toLowerCase();
-console.log(thirdAnswer.toLowerCase());
-if (thirdAnswer ==='y'||'yes') {
-    alert('You rock,' + userName + '. Terra is a mom of two boys.')
+//user being asked the second question
+answer = prompt(userName + ', do you think Terra was a collegiate athlete?');
+
+//checking to see if the second answer matches the answers in the array
+if (answer.toLowerCase() === possibleAnswers[0, 1]) {
+    alert('Correct. Terra was on the swim team at UNI as well as she played rugby. Go Panthers!');
     score++;
 } else {
-    alert('Sorry,' + userName + ' no point for you.')
+    alert('Wa wa. Sorry,' + userName + ' but Terra actually swam for UNI as well as rugby for UNI.');
 };
+console.log('Terra a collegiate athlete?  Expect yes');
+console.log(answer.toLowerCase());
+console.log('Points earned?');
 console.log(score);
-alert( userName + ', you have '+ score + ' points.')
+alert(userName + ' you have ' + score + ' points.');
 
-var fourthAnswer = prompt('Does Terra bring a Mac to class?');
-fourthAnswer.toLowerCase();
-console.log(fourthAnswer.toLowerCase());
-if (fourthAnswer === 'n' || 'no') {
-    alert( userName + ', you are on FIRE! Terra uses a pc.')
+//checking to see if the third answer matches the answers in the array
+answer = prompt('Is Terra a mom?');
+//answer is yes
+//checking to see if userName answers match correct choice
+if (answer.toLowerCase() === possibleAnswers[0, 1]) {
+    alert('You rock,' + userName + '. Terra is a mom of two boys.');
     score++;
 } else {
-    alert('Silly,' + username + 'Terra doesn\'t bring a Mac.')
+    alert('Sorry,' + userName + ' no point for you. Terra has two boys');
 };
+console.log('Is Terra a mom? Expect yes');
+console.log(answer.toLowerCase());
+console.log('Points earned?');
 console.log(score);
-alert('You have '+ score + ' points. Last question!')
+alert(userName + ', you have ' + score + ' points.');
 
-var fifthAnswer = prompt('Does Terra drink coffee?');
-fifthAnswer.toLowerCase();
-console.log(fifthAnswer.toLowerCase());
-
-if (fifthAnswer === 'y' || 'yes') {
-    alert(username + ', you guessed right! Terra LOVES coffee!')
+//checking to see if the fourth answer matches the answers in the array
+answer = prompt('Does Terra bring a Mac to class?');
+//answer is no
+//checking to see if user answer match correct choice
+if (answer.toLowerCase() === possibleAnswers[2, 3]) {
+    alert(userName + ', you are on FIRE! Terra uses a pc.');
     score++;
 } else {
-    alert('What, who doesn\'t love coffee?')
+    alert('Silly,' + userName + ' Terra doesn\'t bring a Mac.');
 };
+console.log('brings Mac to class? Expect no');
+console.log(answer.toLowerCase());
+console.log('Points earned?');
 console.log(score);
-alert(userName + 'you earned'+ score + ' points.')
+alert('You have ' + score + ' points. Question 5 coming up!');
+
+//asking fifth question
+answer = prompt('Does Terra drink coffee?');
+
+//checking to see if the fifth answer matches the answers in the array
+//answer is yes
+if (answer.toLowerCase() === possibleAnswers[0, 1]) {
+    alert(userName + ', you guessed right! Terra LOVES coffee!');
+    score++;
+} else {
+    alert('What, who doesn\'t love coffee?');
+};
+console.log('Terra drinks coffee? Expect yes');
+console.log(answer.toLowerCase());
+console.log('Points earned?');
+console.log(score);
+alert(userName + ' you currently have ' + score + ' points.');
+
+//Asking sixth question.  Answer is an integer and must give four tries
+
+//checking to see if the sixth answer matches the answer 
+   var answerSix = prompt('How many degrees from college has Terra earned?');
+   for (var i = 0; i < 4; i++) {
+    if (answerSix === 3) {
+        console.log('Number of degrees earned? Expect 3');
+        console.log(answerSix);
+        i = 5;
+        alert(userName + ', you guessed right.  Terra has three degrees. Read more about it on her About Me page.');
+        score++;
+    }else{
+        answerSix = prompt('Try again');
+        console.log('Number of degrees earned? Expect 3');
+        console.log(answerSix);
+        }
+   };  
+console.log('Points earned?');
+console.log(score);
+alert(userName + ' you currently have ' + score + 'points.');
+
+//Asking seventh question.  Multiple possible answers
+answer = prompt('What are Terra\'s favorite fruits?');
+var questionSeven = ['blackberries', 'blueberries', 'raspberries', 'strawberries']
+
+for(var i = 0; i <= 6; i++ ) {
+   if (answer.toLowerCase() === questionSeven[0,1]){
+       console.log('Favorite foods? expect blackberries, blueberries, raspberries or strawberries');
+       console.log(answer);
+       alert('Great job ' + userName + ', you guessed right. Terra loves ' + answer);
+       score++;
+       i = 7;
+}else{
+    console.log('Favorite foods? expect blackberries, blueberries, raspberries or strawberries');
+    console.log(answer);
+    prompt('Try again');
+};
+};
+console.log('Points earned?');
+console.log(score);
+alert(userName + ' you earned ' + score + 'points.');
